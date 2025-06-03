d8180db04fe09072fd3484d7b5c233d06fb05dad
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-03 06:56:40 +0500

    a

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -270,18 +270,6 @@
 #                 return False
 #         return True
 
-# # Решение Edit Distance (Расстояние Левенштейна)
-     
-#         # Заполняем таблицу
-#         for i in range(1, m + 1):
-#             for j in range(1, n + 1):
-#                 if word1[i-1] == word2[j-1]:
-#                     dp[i][j] = dp[i-1][j-1]
-#                 else:
-#                     dp[i][j] = min(dp[i-1][j],    # удаление
-#                                  dp[i][j-1],    # вставка
-#                                  dp[i-1][j-1]) + 1  # замена
-#         return dp[m][n]
 
 # # Решение Sliding Window Maximum
 # class SlidingWindowMaximum:
