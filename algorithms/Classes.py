e2f8d25fa27e6590e8cceb49aef7b9d1274e34cf
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-03 06:56:13 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -271,17 +271,7 @@
 #         return True
 
 # # Решение Edit Distance (Расстояние Левенштейна)
-# class EditDistance:
-#     def minDistance(self, word1, word2):
-#         m, n = len(word1), len(word2)
-#         dp = [[0] * (n + 1) for _ in range(m + 1)]
-        
-#         # Инициализация первой строки и столбца
-#         for i in range(m + 1):
-#             dp[i][0] = i
-#         for j in range(n + 1):
-#             dp[0][j] = j
-            
+     
 #         # Заполняем таблицу
 #         for i in range(1, m + 1):
 #             for j in range(1, n + 1):
