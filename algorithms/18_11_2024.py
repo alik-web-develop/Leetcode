5b66595d9521a14872f34874537069d5bd12b9b9
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-04 13:40:38 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -154,22 +154,3 @@ class WordBreak:
                     break
         return dp[len(s)]
 
-# Решение Longest Consecutive Sequence
-# class LongestConsecutiveSequence:
-#     def longestConsecutive(self, nums):
-#         num_set = set(nums)
-#         max_length = 0
-        
-#         for num in num_set:
-#             if num - 1 not in num_set:  # Находим начало последовательности
-#                 current_num = num
-#                 current_length = 1
-                
-#                 while current_num + 1 in num_set:
-#                     current_num += 1
-#                     current_length += 1
-                    
-#                 max_length = max(max_length, current_length)
-                
-#         return max_length
-
