c2a4ab54d6665ada19b42eb3192e3664bdcf415d
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-08 05:41:08 +0500

    /

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -113,13 +113,6 @@
 #     unique = FirstUniqueCharacter()
 #     print(unique.firstUniqChar("leetcode"))  # 0
 
-#     # Rotate Array
-#     rotate = RotateArray()
-#     nums = [1, 2, 3, 4, 5, 6, 7]
-#     rotate.rotate(nums, 3)
-#     print(nums)  # [5, 6, 7, 1, 2, 3, 4]
-
-
 
 
 
