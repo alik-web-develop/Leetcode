047e25138d5ee3d49aacc94f23293486c7cd2f47
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-11 12:31:21 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -312,11 +312,4 @@
 #         return longest
 #         # return result
 # Решение Binary Tree Level Order Traversal
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-
 
