da144d7a474cd2f8ebd14a13fd8a64fd767005a4
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-11 12:31:38 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -295,21 +295,4 @@
 #             # Добавляем максимум в результат
 #             if i >= k - 1:
 #                 result.append(nums[d[0]])
-# Решение Longest Consecutive Sequence
-# class LongestConsecutiveSequence:
-#     def longestConsecutive(self, nums):
-#         num_set = set(nums)
-#         longest = 0
-#         for num in nums:
-#             if num - 1 not in num_set:
-#                 current_num = num
-#                 current_streak = 1
-                
-#                 while current_num + 1 in num_set:
-#                     current_num += 1
-#                     current_streak += 1
-#                 longest = max(longest, current_streak)
-#         return longest
-#         # return result
-# Решение Binary Tree Level Order Traversal
 
