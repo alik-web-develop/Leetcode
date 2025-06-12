3120b436801661fc93869decb4815114664e85ca
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-12 18:48:23 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -315,5 +315,37 @@ class ContainerWithMostWater:
                 
         return max_area
 
+# Решение 3Sum
+class ThreeSum:
+    def threeSum(self, nums):
+        nums.sort()
+        result = []
+        
+        for i in range(len(nums) - 2):
+            # Пропускаем дубликаты
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                
+                if current_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    # Пропускаем дубликаты
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif current_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+                    
+        return result
+
 
 
