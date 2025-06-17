713a494a3fb594622055ec64949e0084513c4f97
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-17 12:12:37 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -27,3 +27,11 @@
 # # Пример использования:
 # # print(factorial(5))  # Выведет: 120
 
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
+
