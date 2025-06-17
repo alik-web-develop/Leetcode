c59cb6496e9a2b6d9aafcc3ff9e5b623dc9c9a2c
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-18 02:50:52 +0500

    add algorithm

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -688,6 +688,21 @@ class Dijkstra:
                     
         return distances
 
+# Решение Knapsack Problem
+class Knapsack:
+    def knapsack(self, weights, values, capacity):
+        n = len(weights)
+        dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            for w in range(capacity + 1):
+                if weights[i-1] <= w:
+                    dp[i][w] = max(values[i-1] + dp[i-1][w-weights[i-1]], dp[i-1][w])
+                else:
+                    dp[i][w] = dp[i-1][w]
+                    
+        return dp[n][capacity]
+
 
     
 
