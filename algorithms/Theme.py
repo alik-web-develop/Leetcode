0190df713c2f42c00c7d53efabe905901676e024
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-17 11:17:01 +0500

    add single_number

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -8,6 +8,13 @@ def max_subarray(nums):
         max_sum = max(max_sum, curr_sum)
     return max_sum
 
+def single_number(nums):
+    result = 0
+    for num in nums:
+        result ^= num
+    return result
+
 # Пример использования:
 # print(reverse_string("leetcode"))  # Выведет: "edocteel"
-# print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))  # Выведет: 6 
+# print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))  # Выведет: 6
+# print(single_number([4,1,2,1,2]))  # Выведет: 4 
