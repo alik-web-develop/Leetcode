3b91ce7c1ae87659f1093697e4a2f21a5af9ca88
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-17 12:11:56 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -1,2 +1,10 @@
 # # l = ["Hello", "abc","test","a","ab","world"]
 
+# # def bubble_sort(u_ns):
+# #     sorted = False
+# #     while not sorted:
+# #         sorted = True
+# #         for i in range(len(u_ns)-1):
+# #             if len(u_ns[i]) > len(u_ns[i+1]):
+# #                 u_ns[i], u_ns[i + 1] = u_ns[i+1], u_ns[i]
+# #                 sorted = False
