885fc7e6c490d342cd4962e42bf7089dc9f893d5
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-19 20:27:49 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -10,7 +10,7 @@
 # #                 sorted = False
 
 # # bubble_sort(l)
-# # print(l) 
+# # print(l)
 # # def pyr(num,num2=1):
 # #     print(" "*num2 + "*"*(num-num2))
 # #     if num > num2:
@@ -80,4 +80,56 @@
 
 # # Пример использования:
 # # print(two_sum([2, 7, 11, 15], 9))  # Выведет: [0, 1]
-        
+
+class Bank:
+    def __init__(self, bank_name: str, account_id: int, pin: int):
+        self._bank_name = bank_name
+        self._bank_id = account_id
+        self._pin = pin
+        self._balance = 0
+
+    def __str__(self):
+        return f"You have a account on this bank- '{self._bank_name}'"
+
+    def print_msg(self, msg):
+        print("\n")
+        print("*" * 20)
+        print(msg)
+        print("*" * 20)
+        print("\n")
+
+    def validate_account_exist(self, account_id, pin) -> bool:
+        if self._bank_id == account_id and self._pin == pin:
+            return True
+        self.print_msg(f"No account exist on id {account_id}")
+        return False
+
+    def invest(self, amount: float, account_id: int, pin: int) -> bool:
+        """Вклад в личный счет"""
+        if self.validate_account_exist(account_id, pin):
+            self._balance = self._balance + amount
+            self.print_msg(
+                f" SUCCESFULT INVEST your new balance {self._balance}")
+            return True
+        return False
+
+    def withdraw(self, amount: float, account_id: int, pin: int) -> bool:
+        if self.validate_account_exist(account_id, pin):
+            self._balance = self._balance - amount
+            self.print_msg(
+                f" SUCCESLUFY WITHDRAW your new balance {self._balance}")
+            return True
+
+
+class HamkorBank(Bank):
+    def __init__(self, bank_name: str, account_id: int, pin: int, location: str):
+        super().__init__(bank_name, account_id, pin)
+        self.location = location
+
+
+account_id = 123908  # int!
+pin = 123123         # int!
+bank_balance = HamkorBank("Hamkor bank", account_id, pin, "Amir temur 30 st: Registan")
+print(bank_balance)
+
+bank_balance.invest(30000.00, account_id, pin)
