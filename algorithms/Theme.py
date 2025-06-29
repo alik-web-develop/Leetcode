5b0d131124b5025ae7973ece93f44d758f9fabe5
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-29 13:14:38 +0500

    /

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -134,3 +134,16 @@ print(bank_balance)
 
 bank_balance.invest(30000.00, account_id, pin)
 
+# Binary Search
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
