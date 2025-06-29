743c69a3d10cf107c53c519834ee9839f7743521
alik-web-develop <alijongulyamov03@gmail.com>
2025-06-29 13:14:43 +0500

    /

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -147,3 +147,13 @@ def binary_search(arr, target):
             right = mid - 1
     return -1
 
+# Quick Sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
