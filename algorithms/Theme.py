3f2d50aa3480f02ccfa73cd88008424f68cae285
alik-web-develop <alijongulyamov03@gmail.com>
2025-07-06 03:17:25 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -299,4 +299,27 @@ def topological_sort(graph):
 # }
 # print(topological_sort(graph)) # Выведет: ['A', 'B', 'C', 'D', 'E'] или ['B', 'A', 'C', 'D', 'E']
 
+# Longest Common Subsequence (LCS) (для нахождения самой длинной общей подпоследовательности)
+def lcs(X, Y):
+    m = len(X)
+    n = len(Y)
+
+    L = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1] + 1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+    return L[m][n]
+
+# Пример использования:
+# X = "AGGTAB"
+# Y = "GXTXAYB"
+# print(lcs(X, Y)) # Выведет: 4 (GTAB)
+
+
 
