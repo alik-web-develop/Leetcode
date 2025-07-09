50470d9a89146341be44826e4d8753c8ce328083
alik-web-develop <alijongulyamov03@gmail.com>
2025-07-09 23:53:38 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -874,3 +874,22 @@ def gcd_euclidean(a, b):
 
 
 # Shell Sort (Сортировка Шелла)
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+# Пример использования:
+# arr = [12, 34, 54, 2, 3]
+# print(shell_sort(arr)) # Выведет: [2, 3, 12, 34, 54]
+
