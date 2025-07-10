55fc4a05f0cc24ee152307e388f3fa9fb01a1293
alik-web-develop <alijongulyamov03@gmail.com>
2025-07-11 00:56:01 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -911,4 +911,22 @@ def selection_sort(arr):
 # arr = [64, 25, 12, 22, 11]
 # print(selection_sort(arr)) # Выведет: [11, 12, 22, 25, 64]
 
+# =================
+
+# Insertion Sort (Сортировка вставками)
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# Пример использования:
+# arr = [12, 11, 13, 5, 6]
+# print(insertion_sort(arr)) # Выведет: [5, 6, 11, 12, 13]
+
+
 
