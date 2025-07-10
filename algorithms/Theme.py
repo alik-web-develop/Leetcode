afae1371f8090f5c506268e9c86c2fd7d37e44a5
alik-web-develop <alijongulyamov03@gmail.com>
2025-07-11 00:56:15 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -950,5 +950,45 @@ def counting_sort(arr):
 # arr = [4, 2, 2, 8, 3, 3, 1]
 # print(counting_sort(arr)) # Выведет: [1, 2, 2, 3, 3, 4, 8]
 
+# =================
+
+# Radix Sort (Поразрядная сортировка)
+def counting_sort_for_radix(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10 # Для цифр от 0 до 9
+
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+    
+    for i in range(n):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    if not arr:
+        return []
+
+    max_val = max(arr)
+    exp = 1
+    while max_val // exp > 0:
+        counting_sort_for_radix(arr, exp)
+        exp *= 10
+    return arr
+
+# Пример использования:
+# arr = [170, 45, 75, 90, 802, 24, 2, 66]
+# print(radix_sort(arr)) # Выведет: [2, 24, 45, 66, 75, 90, 170, 802]
+
 
 
