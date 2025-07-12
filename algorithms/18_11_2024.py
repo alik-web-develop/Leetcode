cc18bd8a2ef617993335b0e9fd66be2765ffd7ed
alik-web-develop <alijongulyamov03@gmail.com>
2025-07-12 09:42:23 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -133,3 +133,30 @@ class MaximumSubarray:
 # max_sub = MaximumSubarray()
 # print(max_sub.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))  # 6
 
+# ===============================================
+# 2. Merge Two Sorted Lists
+# You are given the heads of two sorted linked lists list1 and list2.
+# Merge the two lists into one sorted list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MergeSortedLists:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(0)
+        current = dummy
+        
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        
+        current.next = list1 if list1 else list2
+        return dummy.next
+
+
