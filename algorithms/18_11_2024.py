a17a2c6438f2e5e97856a94b63f663b5876380b7
alik-web-develop <alijongulyamov03@gmail.com>
2025-07-12 09:42:39 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -208,6 +208,30 @@ class ValidPalindromeII:
 # palindrome_ii = ValidPalindromeII()
 # print(palindrome_ii.validPalindrome("abca"))  # True
 
+# ===============================================
+# 5. Move Zeroes
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+class MoveZeroes:
+    def moveZeroes(self, nums):
+        non_zero_index = 0
+        
+        # Перемещаем все ненулевые элементы в начало
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+        
+        # Заполняем оставшиеся позиции нулями
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0
+        
+        return nums
+
+# Пример использования:
+# move_zeros = MoveZeroes()
+# nums = [0,1,0,3,12]
+# move_zeros.moveZeroes(nums)
+# print(nums)  # [1,3,12,0,0]
 
 
 
