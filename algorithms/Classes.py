691b0e0f97f6b41e83f7e4bd695bab1369bdd2bf
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-08-01 13:31:01 +0500

    ,

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -24,7 +24,6 @@
 # # # # # # # #         return not stack
 
 # # # # # # # # Решение Reverse Linked List
-# # # # # # # # class ListNode:
 # # # # # # # #     def __init__(self, val=0, next=None):
 # # # # # # # #         self.val = val
 # # # # # # # #         self.next = next
