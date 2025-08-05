ad2365ba65109c05585e5c6978f2c40cda9ea776
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-08-05 08:44:55 +0500

    M

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -20,12 +20,6 @@
 # # # # # # # #     def __init__(self, val=0, next=None):
 # # # # # # # #         self.val = val
 # # # # # # # #         self.next = next
-
-# # # # # # # # class ReverseLinkedList:
-# # # # # # # #     def reverseList(self, head):
-# # # # # # # #         prev = None
-# # # # # # # #         current = head
-# # # # # # # #         while current:
 # # # # # # # #             next_temp = current.next
 # # # # # # # #             current.next = prev
 # # # # # # # #             prev = current
