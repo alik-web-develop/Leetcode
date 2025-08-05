7719a969e829036779aaf19b4cc1d969c34ac8d4
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-08-05 08:44:05 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -15,8 +15,6 @@
 # # # # # # # #         stack = []
 # # # # # # # #         mapping = {')': '(', '}': '{', ']': '['}
 # # # # # # # #         for char in s:
-# # # # # # # #             if char in mapping:
-# # # # # # # #                 top_element = stack.pop() if stack else '#'
 # # # # # # # #                 if mapping[char] != top_element:
 # # # # # # # #                     return False
 # # # # # # # #             else:
