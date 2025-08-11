c0fede5c12363ea67771b69114885496aa040a2c
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-08-11 12:32:55 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -23,8 +23,6 @@
 # # # # # # # #     def mergeTwoLists(self, l1, l2):
 # # # # # # # #         dummy = ListNode(0)
 # # # # # # # #         current = dummy
-# # # # # # # #         while l1 and l2:
-# # # # # # # #             if l1.val < l2.val:
 # # # # # # # #                 current.next = l1
 # # # # # # # #                 l1 = l1.next
 # # # # # # # #             else:
