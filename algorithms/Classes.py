77e82b4307391228a50373dce2683c1c35afdc4a
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-08-15 21:34:03 +0500

    m

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -20,6 +20,7 @@
 
 # # # # # # # # Решение Merge Two Sorted Lists
 # # # # # # # #         current = dummy
+# # # # # # # #         while l1 and l2:
 # # # # # # # #                 current.next = l1
 # # # # # # # #                 l1 = l1.next
 # # # # # # # #                 l2 = l2.next
