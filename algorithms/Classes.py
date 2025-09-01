756aa4e8f8badcd9f611d24b0f4fa9225757f37f
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-09-01 08:38:40 +0500

    m

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -25,7 +25,6 @@
 # # # # # # # #         return dummy.next
 
 # # # # # # # #         left, right = 0, len(nums) - 1
-# # # # # # # #         while left <= right:
 # # # # # # # #             mid = (left + right) // 2
 # # # # # # # #             if nums[mid] == target:
 # # # # # # # #                 return mid
