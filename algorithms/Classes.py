d319e53802a120f24970de1de08e70326bb10397
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-09-01 08:38:55 +0500

    m

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -27,7 +27,6 @@
 # # # # # # # #             if nums[mid] == target:
 # # # # # # # #                 return mid
 # # # # # # # #             elif nums[mid] < target:
-# # # # # # # #                 left = mid + 1
 # # # # # # # #             else:
 # # # # # # # #                 right = mid - 1
 # # # # # # # #         return -1
