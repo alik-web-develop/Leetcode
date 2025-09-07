f45fa2f66b02862a407105a2ddb4c9079bdeb228
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-09-07 18:13:52 +0500

    m

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -656,5 +656,3 @@
 #                     dp[i][w] = max(values[i-1] + dp[i-1][w-weights[i-1]], dp[i-1][w])
 #                 else:
 #                     dp[i][w] = dp[i-1][w]
-                    
-#         return dp[n][capacity]
