4ab31890d132bb92409e4bba91521a9ef078464a
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-09-21 20:17:28 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -576,5 +576,3 @@
             
 #         pivot = arr[len(arr) // 2]
 #         left = [x for x in arr if x < pivot]
-#         middle = [x for x in arr if x == pivot]
-#         right = [x for x in arr if x > pivot]
