e80e2cb477a3d66432caedb8569d688169c26870
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-04 12:47:10 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -449,9 +449,6 @@
 # # #         left = self.lowestCommonAncestor(root.left, p, q)
 # # #         right = self.lowestCommonAncestor(root.right, p, q)
         
-# # #         if left and right:
-# # #             return root
-# # #         return left if left else right
 
 # # # Решение Combination Sum
 # # class CombinationSum:
