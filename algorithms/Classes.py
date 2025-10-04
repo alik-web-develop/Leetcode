d26e0ad94316f94e831905dff1e148a43c6b164e
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-04 12:47:30 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -445,5 +445,3 @@
 # # #     def lowestCommonAncestor(self, root, p, q):
 # # #         if not root or root == p or root == q:
 # # #             return root
-            
-# # #         left = self.lowestCommonAncestor(root.left, p, q)
