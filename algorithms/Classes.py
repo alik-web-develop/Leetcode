8c33b9d24e2947fcda8674661d4d9da6214b55cb
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-13 20:31:47 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -345,8 +345,6 @@
 # # #         for i in range(len(p), len(s)):
 # # #             s_count[ord(s[i]) - ord('a')] += 1
 # # #             s_count[ord(s[i - len(p)]) - ord('a')] -= 1
-            
-# # #             if p_count == s_count:
 # # #                 result.append(i - len(p) + 1)
                 
 # # #         return result
