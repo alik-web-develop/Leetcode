3fe7f5e349eb9f743b88f66f065a783d1da631e4
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-13 20:31:37 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -369,8 +369,6 @@
 # # #                 if (num in rows[i] or 
 # # #                     num in cols[j] or 
 # # #                     num in boxes[box_idx]):
-# # #                     return False
-
 # numbers = [4,6,2,6,8,9,1,1,12]
 # def target_num(nums,target):
 #     result = []
