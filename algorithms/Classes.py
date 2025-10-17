a1b1532f0b38365773f10ba69a5de83d25539144
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-17 20:50:22 +0500

    ,

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -344,9 +344,6 @@
 # # #             for j in range(9):
 # # #                 num = board[i][j]
 # # #                     continue
-                    
-# # #                 box_idx = (i // 3) * 3 + j // 3
-                
 # # #                 if (num in rows[i] or 
 # numbers = [4,6,2,6,8,9,1,1,12]
 # def target_num(nums,target):
