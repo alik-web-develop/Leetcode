81d25720373628259169bad51e08facedab0dcb4
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-22 01:38:22 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -203,8 +203,6 @@
 #     def moveZeroes(self, nums):
 #         non_zero_index = 0
         
-#         # Перемещаем все ненулевые элементы в начало
-#         for i in range(len(nums)):
 #             if nums[i] != 0:
 #                 nums[non_zero_index] = nums[i]
 #                 non_zero_index += 1
