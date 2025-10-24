477835cb3dff74ba7125dbe14c66d1d0cb541afd
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-25 02:04:13 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -187,9 +187,6 @@
         
 #         left, right = 0, len(s) - 1
         
-#         while left < right:
-#             if s[left] != s[right]:
-#                 return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
 # class MoveZeroes:
 #                 non_zero_index += 1
         
