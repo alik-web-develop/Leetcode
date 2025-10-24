5bf7ca61bba08ba264836039135d2eaadb73d647
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-25 02:05:25 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -163,5 +163,3 @@
 #         max_profit = 0
         
 #         for price in prices[1:]:
-#             min_price = min(min_price, price)
-#             max_profit = max(max_profit, price - min_price)
