0c23509a56170082b3b771ffa3c0819510547b05
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-10-26 01:10:35 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -153,8 +153,4 @@
 # # ===============================================
 # # 3. Best Time to Buy and Sell Stock
 # # You are given an array prices where prices[i] is the price of a given stock on the ith day.
-# # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-# class BestTimeToBuySell:
-#     def maxProfit(self, prices):
-#         if not prices:
 #             return 0
