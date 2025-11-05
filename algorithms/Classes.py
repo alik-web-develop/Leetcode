0b0f3b06ec45172ea5fde4496b213f289ce6b07b
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-11-05 15:17:06 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -282,7 +282,5 @@
 # # # #             if i > 0 and nums[i] == nums[i-1]:
 # # # #                 continue
                 
-# # # #             left, right = i + 1, len(nums) - 1
-            
-# # # #             while left < right:
 # # # #                 current_sum = nums[i] + nums[left] + nums[right]
+
