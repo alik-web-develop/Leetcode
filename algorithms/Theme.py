a65d5340e573e18db5426d9957a9f4e86260bbac
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-11-10 23:31:24 +0500

    ,

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -1376,9 +1376,3 @@
 
 # # Пример использования:
 # # graph = {0: [1, 2], 1: [0, 2], 2: [0, 1, 3], 3: [2, 4], 4: [3]}
-# # print(find_bridges(graph)) # [(3, 4), (2, 3)]
-
-# # =================
-
-# # 3. Поиск точек сочленения (articulation points)
-# def find_articulation_points(graph):
