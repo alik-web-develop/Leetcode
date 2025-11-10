1f6c8154c946570c3dbd9a0e79a982e039818255
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-11-10 08:32:19 +0500

    .

--- algorithms/18_11_2024.py
+++ algorithms/18_11_2024.py
@@ -14,10 +14,3 @@
 # # # 92. Reverse Linked List II
 # # # Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 # # # x = [1,2,3,4,5,6,7,8,9,10]
-# # # def reverse_linked(arr,left,right):
-# # #     else:
-
-# # # вот тут литкод мой код не принял так как к нам оказывается приходит не просто list а ListNode это то ещё проблема так как там нету индексации типо вы не можете использовать element[left:right] из за этого пришлось голову морочить вот короче код который литкод принял
-# # class Solution:
-# #     def reverseBetween(self, head, left: int, right: int):
-# #         dummy = ListNode(0)
