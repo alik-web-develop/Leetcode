c12efb043fa813d735acce370eb9a9a5caf37113
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-11-12 04:45:23 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -1337,9 +1337,3 @@
 #     path.reverse()
 #     if path and path[0] == start:
 #         return path
-#     return []
-
-# # Пример использования:
-# # graph = {'A': {'B': 1, 'C': 4}, 'B': {'A': 1, 'C': 2, 'D': 5}, 'C': {'A': 4, 'B': 2, 'D': 1}, 'D': {'B': 5, 'C': 1}}
-# # dist, prev = dijkstra_with_path(graph, 'A')
-# # print(dist)
