82101dd55efdbb8752df5710e643866cecc679e9
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-12-08 22:53:30 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -146,10 +146,4 @@
 # # # # # # # #         while queue:
 # # # # # # # #             word, length = queue.pop(0)
 # # # # # # # #             if word == endWord:
-# # # # # # # #                 return length
-                
-# # # # # # # #             for i in range(len(word)):
-# # # # # # # #                 for c in 'abcdefghijklmnopqrstuvwxyz':
-# # # # # # # #                     next_word = word[:i] + c + word[i+1:]
-# # # # # # # #                     if next_word in wordList and next_word not in visited:
-# # # # # # # #                         queue.append((next_word, length + 1))
+#=
