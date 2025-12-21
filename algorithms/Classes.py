ebbf5bc25d41aaddf9eec496e8e969d5aa928ba8
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-12-21 21:17:57 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -67,5 +67,3 @@
 # # # # # # # #         non_zero = 0
 # # # # # # # #         for i in range(len(nums)):
 # # # # # # # #             if nums[i] != 0:
-# # # # # # # #                 nums[non_zero], nums[i] = nums[i], nums[non_zero]
-# # # # # # # #                 non_zero += 1
