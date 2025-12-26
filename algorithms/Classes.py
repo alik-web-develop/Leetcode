02daf586f6818a3a338904ff1a94cf71ea50a3eb
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-12-26 22:00:54 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -35,5 +35,4 @@
 # # # # # # # #         dp[1] = 1
 # # # # # # # #         dp[2] = 2
 # # # # # # # #         for i in range(3, n + 1):
-# # # # # # # #             dp[i] = dp[i-1] + dp[i-2]
-# # # # # # # #         return dp[n]
+# # # # # # # #             dp[i] = dp[i-1] + dp[i# # # # # # # #         return dp[n]
