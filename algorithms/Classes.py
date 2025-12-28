56108a622133eb9f3d282cf92ec9ecdf5d6803e1
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2025-12-28 22:03:48 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -30,7 +30,3 @@
 # # # # # # # # Решение Climbing Stairs
 # # # # # # # # class ClimbingStairs:
 # # # # # # # #         if n <= 2:
-# # # # # # # #             return n
-# # # # # # # #         dp = [0] * (n + 1)
-# # # # # # # #         dp[1] = 1
-# # # # # # # #         dp[2] = 2
