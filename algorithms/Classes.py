7caeb5ed1fb7f4801b4e8d1f4338c71a9f35040e
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-01 08:08:54 +0500

    .

--- algorithms/Classes.py
+++ algorithms/Classes.py
@@ -3,5 +3,3 @@
 # # # # # # # #     def twoSum(self, nums, target):
 # # # # # # # #         hash_map = {}
 # # # # # # # #         for i, num in enumerate(nums):
-# # # # # # # #             complement = target - num
-# # # # # # # #             if complement in hash_map:
