10aaec536247a0824049625eaa6d6fe018ca6807
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-13 22:44:52 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -1100,5 +1100,4 @@
 # #     for i in range(n - m + 1):
 # #         if pattern_hash == text_hash:
 # #             ``        if text[i:i+m] == pattern:
-# #                 occurrences.append(i)
-        
+# #                 occurrences.a
