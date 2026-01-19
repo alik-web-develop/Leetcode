92e3200ab9c66e96caf8ffe60986e9aae9035b59
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-19 22:52:10 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -981,15 +981,3 @@
 
 # # #     max_val = max(arr)
 # # #     exp = 1
-# # #     while max_val // exp > 0:
-# # #         counting_sort_for_radix(arr, exp)
-# # #         exp *= 10
-# # #     return arr
-
-# # # # Пример использования:
-# # # # arr = [170, 45, 75, 90, 802, 24, 2, 66]
-# # # # print(radix_sort(arr)) # Выведет: [2, 24, 45, 66, 75, 90, 170, 802]
-
-# # # # =================
-
-# # # # Heap Sort (Пирамидальная сортировка)
