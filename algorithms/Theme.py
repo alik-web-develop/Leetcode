b0c1f7e348eaefecbf59901a477927fbfdd663dc
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-19 22:52:02 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -993,16 +993,3 @@
 # # # # =================
 
 # # # # Heap Sort (Пирамидальная сортировка)
-# # # def heapify(arr, n, i):
-# # #     largest = i
-# # #     l = 2 * i + 1
-# # #     r = 2 * i + 2
-
-# # #     if l < n and arr[l] > arr[largest]:
-# # #         largest = l
-    
-# # #     if r < n and arr[r] > arr[largest]:
-# # #         largest = r
-    
-# # #     if largest != i:
-# # #         arr[i], arr[largest] = arr[largest], arr[i]
