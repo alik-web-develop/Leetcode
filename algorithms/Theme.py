e6b70b0d10c7e82022721e5f2a19a4e51c61c6d7
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-23 23:08:02 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -919,7 +919,4 @@
 # # #         key = arr[i]
 # # #         j = i - 1
 # # #         while j >= 0 and key < arr[j]:
-# # #             arr[j + 1] = arr[j]
-# # #             j -= 1
-# # #         arr[j + 1] = key
-# # #     return arr
+# # #             ar
