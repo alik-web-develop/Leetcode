917e7f446107d3b56731f8b6b95713c7e7498c6c
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-24 22:01:16 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -904,5 +904,4 @@
 # # #         for j in range(i + 1, n):
 # # #             if arr[j] < arr[min_idx]:
 # # #                 min_idx = j
-# # #         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-# # #     return arr
+# # #         arr[i], arr[min_idx] = arr[min_idx], arr[
