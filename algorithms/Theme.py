44cd45149b077b1c5b9ab542eca2c45bf25ecb2a
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-25 23:36:02 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -879,10 +879,4 @@
 # # #     gap = n // 2
 
 # # #     while gap > 0:
-# # #         for i in range(gap, n):
-# # #             temp = arr[i]
-# # #             j = i
-# # #             while j >= gap and arr[j - gap] > temp:
-# # #                 arr[j] = arr[j - gap]
-# # #                 j -= gap
-# # #             arr[j] = t
+# # #         for i in 
