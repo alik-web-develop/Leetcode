b62e1378e6cacd39efdebda53289d3dac14ac794
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-26 20:49:00 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -824,9 +824,3 @@
 # # #             lps[i] = length
 # # #             i += 1
 # # #         else:
-# # #             if length != 0:
-# # #                 length = lps[length - 1]
-# # #             else:
-# # #                 lps[i] = 0
-# # #                 i += 1
-# # #     return lps
