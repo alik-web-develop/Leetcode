1f9490104b3a157961ec8dea22103213b5c406f3
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-01-30 20:29:50 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -24,13 +24,7 @@
 # # # # # # #         return 1
 # # # # # # #     return n * factorial(n-1)
 
-# # # # # # # # Пример использования:
-# # # # # # # # print(factorial(5))  # Выведет: 120
-
-# # # # # # # def is_prime(n):
-# # # # # # #     if n < 2:
-# # # # # # #         return False
-# # # # # # #     for i in range(2, int(n ** 0.5) + 1):
+# # # # # # # # Пример исn range(2, int(n ** 0.5) + 1):
 # # # # # # #         if n % i == 0:
 # # # # # # #             return False
 # # # # # # #     return True
