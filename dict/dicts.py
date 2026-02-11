b5a0952066af2bf391172dae559fa7b90f82d4fc
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-02-11 22:17:26 +0500

    ,

--- dict/dicts.py
+++ dict/dicts.py
@@ -10,8 +10,3 @@
 # #                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
 # #     return arr
 #h)
-#     return resultду двумя словарями по ключам и значениям.
-    
-#     Returns:es
-#     test_dict = {
-#         'a': 1,\
