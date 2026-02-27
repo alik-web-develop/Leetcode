ff415a90aff7ddf78a75d039de82664059eeda6c
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-02-27 15:25:31 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -493,11 +493,3 @@
 # # # # #                 dp[i][w] = max(values[i-1] + dp[i-1][w - weights[i-1]], dp[i-1][w])
 # # # # #             else:
 # # # # #                 dp[i][w] = dp[i-1][w]
-# # # # #     return dp[n][capacity]
-
-# # # # # # Пример использования:
-# # # # # # values = [60, 100, 120]
-# # # # # # weights = [10, 20, 30]
-# # # # # # capacity = 50
-# # # # # # print(knapsack(values, weights, capacity)) # Выведет: 220 (взяты предметы со стоимостью 100 и 120)
-
