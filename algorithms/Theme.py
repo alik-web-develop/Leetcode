6fc9653d76201e8448af7ad5a99be171b175c9ac
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-03-13 23:31:16 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -485,8 +485,4 @@
 # # # # # # Knapsack Problem (0/1 Knapsack) (решение задачи о рюкзаке)
 # # # # # def knapsack(values, weights, capacity):
 # # # # #     n = len(values)
-# # # # #     dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
-
-# # # # #     for i in range(1, n + 1):
-# # # # #         for w in range(1, capacity + 1):
-# # # # #
+# # # # #  
