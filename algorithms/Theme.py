60046d061913547b7457697a7b5b6984791ec24e
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-03-13 23:30:27 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -489,7 +489,4 @@
 
 # # # # #     for i in range(1, n + 1):
 # # # # #         for w in range(1, capacity + 1):
-# # # # #             if weights[i-1] <= w:
-# # # # #                 dp[i][w] = max(values[i-1] + dp[i-1][w - weights[i-1]], dp[i-1][w])
-# # # # #             else:
-# # # # #                 dp[i][w] = dp[i-1][w]
+# # # # #
