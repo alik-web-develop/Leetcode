a564f50c560070bb9c21b3fe48033ea910a3d633
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-03-18 17:17:41 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -319,11 +319,3 @@
 # # # # # # def floyd_warshall(graph):
 # # # # # #     num_vertices = len(graph)
 # # # # # #     dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
-
-# # # # # #     for k in range(num_vertices):
-# # # # # #         for i in range(num_vertices):
-# # # # # #             for j in range(num_vertices):
-# # # # # #                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-# # # # # #     return dist
-
-# # # # # # # Пример использования:
