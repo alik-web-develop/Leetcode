da1d770d2862276ba3afd0d9a295b236b9f8bc59
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-04-08 02:13:52 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -145,9 +145,6 @@
 # # # # # # # # Quick Sort
 # # # # # # # def quick_sort(arr):
 # # # # # # #     if len(arr) <= 1:
-# # # # # # #         return arr
-# # # # # # #     pivot = arr[len(arr) // 2]
-# # # # # # #     left = [x for x in arr if x < pivot]
 # # # # # # #     middle = [x for x in arr if x == pivot]
 # # # # # # #     right = [x for x in arr if x > pivot]
 # # # # # # # # Merge Sort
