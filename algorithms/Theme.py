923d6927f237e958453a29b0d209dc05e4b1ce8e
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-04-08 02:14:00 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -132,8 +132,6 @@
 # # # # # # # Binary Search
 # # # # # # # def binary_search(arr, target):
 # # # # # # #     left, right = 0, len(arr) - 1
-# # # # # # #     while left <= right:
-# # # # # # #         mid = (left + right) // 2
 # # # # # # #         if arr[mid] == target:
 # # # # # # #             return mid
 # # # # # # #         elif arr[mid] < target:
