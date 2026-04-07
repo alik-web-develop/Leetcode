88eba19516c5d107d680985b40ed97d49c0a0064
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-04-07 08:26:41 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -246,9 +246,4 @@
 # # # # # # # Kadane's Algorithm (для нахождения максимальной суммы подмассива)
 # # # # # # def kadane(arr):
 # # # # # #     max_so_far = arr[0]
-# # # # # #     current_max = arr[0]
-
-# # # # # #     for i in range(1, len(arr)):
-# # # # # #         current_max = max(arr[i], current_max + arr[i])
-# # # # # #         max_so_far = max(max_so_far, current_max)
-
+# # # #
