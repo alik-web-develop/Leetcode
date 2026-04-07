03ffe0501abbf6c951d662281660aaf2993af949
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-04-08 02:13:41 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -156,8 +156,6 @@
 # # # # # # # def merge_sort(arr):
 # # # # # # #     if len(arr) <= 1:
 # # # # # # #         return arr
-# # # # # # #     mid = len(arr) // 2
-# # # # # # #     left_half = arr[:mid]
 # # # # # # #     right_half = arr[mid:]
 
 # # # # # # #     left_half = merge_sort(left_half)
