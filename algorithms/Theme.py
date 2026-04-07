fef633c78e67adcc4c8f9ee8098dbbef1b80e38e
Sobiraliyev Alijon <alijongulyamov03@gmail.com>
2026-04-07 08:27:31 +0500

    .

--- algorithms/Theme.py
+++ algorithms/Theme.py
@@ -193,10 +193,4 @@
 # # # # # # #     result = []
 # # # # # # #     while queue:
 # # # # # # #         node = queue.popleft()
-# # # # # # #         result.append(node)
-# # # # # # #         for neighbor in graph.get(node, []):
-# # # # # # #             if neighbor not in visited:
-# # # # # # #                 visited.add(neighbor)
-# # # # # # #                 queue.append(neighbor)
-# # # # # # #     return result
-
+# # #
